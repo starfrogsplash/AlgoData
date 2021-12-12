de23976d664e0d4da6b9791184f0b8d8b82a3068
starfrogsplash <ali_khalil@hotmail.co.uk>
2021-12-12 23:00:35 +0000

    feat: Added Min Max Stack

---
+++ dataStructures/Stacks/MinMaxStack/spec/MinMaxStack.spec.ts
@@ -0,0 +1,13 @@
+import { MinMaxStack } from "../MinMaxStack";
+
+
+it('returns max value, and inserts stack correctly', ()=> {
+    const minMaxStack =  new MinMaxStack()
+
+    minMaxStack.push(5)
+    minMaxStack.push(10)
+    minMaxStack.push(15)
+
+    expect(minMaxStack.stack).toEqual([5,10,15])
+    expect(minMaxStack.minMaxStack[minMaxStack.minMaxStack.length-1 ].max).toEqual(15)
+})
