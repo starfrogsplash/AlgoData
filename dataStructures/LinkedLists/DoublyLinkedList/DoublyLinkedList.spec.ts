a97b552b753f19fd1cd6b8c9c619ba9af7765dca
starfrogsplash <ali_khalil@hotmail.co.uk>
2021-12-13 21:26:36 +0000

    changed: Added Doubly Linked List

--- dataStructures/LinkedLists/DoublyLinkedList/DoublyLinkedList.spec.ts
+++ dataStructures/LinkedLists/DoublyLinkedList/DoublyLinkedList.spec.ts
@@ -1,16 +1,16 @@
-import { Node, DoublyLinkedList } from "./DoublyLinkedList";
+import { DoublyLinkedList } from "./DoublyLinkedList";
 
-it('constructs a doubly linkedList', () => {
-    const dLinkedList = new DoublyLinkedList()
+fit('constructs a doubly linkedList', () => {
+  // Testing functions
+  
+  const dLinkedList = new DoublyLinkedList();
+  
+  dLinkedList.insert(7);
+  dLinkedList.insert(8);
+  dLinkedList.insert(9);
 
-    const node1 = new Node(1)
-    const node2 = new Node(2)
-    const node3 = new Node(3)
+  console.log(dLinkedList)
 
-    dLinkedList.setHead(node1)
-    dLinkedList.setHead(node2)
-    dLinkedList.setTail(node3)
-
-    expect(dLinkedList.head.value).toEqual(2)
-    expect(dLinkedList.tail.value).toEqual(3)
+    // expect(dLinkedList.head.value).toEqual(2)
+    // expect(dLinkedList.tail.value).toEqual(3)
 })
--- dataStructures/LinkedLists/DoublyLinkedList/DoublyLinkedList.ts
+++ dataStructures/LinkedLists/DoublyLinkedList/DoublyLinkedList.ts
@@ -1,113 +1,151 @@
 class Node {
-    value: any
-    prev: any
     next: any
-
+    previous: any
     constructor(value: any) {
-        this.value = value;
-        this.prev = null
-        this.next = null
+      value = value,
+      this.next = null,
+      this.previous =null
     }
-}
-
-class DoublyLinkedList {
-    head: any
-    tail: any
+  }
+  
+  class DoublyLinkedList {
+      head: any
+      tail: any
+      length : number
     constructor() {
-        this.head = null;
-        this.tail = null
+      this.head = null;
+      this.tail = null;
+      this.length = 0;
     }
-
-    setHead(node: Node) {
-        if (this.head === null) {
-            this.head = node
-            this.tail = node
-            return
-        }
-        this.insertBefore(this.head, node)
+  
+    insert(value: any) {
+      this.length++;
+      let newNode = new Node(value);
+  
+      if (this.tail) {
+        this.tail.next = newNode;
+        newNode.previous = this.tail;
+        this.tail = newNode;
+        return newNode;
+      }
+  
+      this.head = this.tail = newNode;
+      return newNode;
     }
-
-    setTail(node: Node) {
-        if (this.tail === null) {
-            this.setHead(node);
-            return
-        }
-        this.insertAfter(this.tail, node)
-    }
-
-    insertBefore(node: Node, nodeToInsert: Node) {
-        if (nodeToInsert === this.head && nodeToInsert === this.tail) return;
-        this.remove(nodeToInsert)
-        nodeToInsert.prev = node.prev;
-        nodeToInsert.next = node.next;
-        if (node.prev === null) {
-            this.head = nodeToInsert;
+  
+    remove() {
+      if (this.tail) {
+        this.length--;
+  
+        const removedTail = this.tail;
+        const beforeTail = this.tail.previous;
+  
+        this.tail = beforeTail;
+        if (this.tail) {
+          this.tail.next = null;
         } else {
-            node.prev.prev = nodeToInsert;
+          this.head = null;
         }
-        node.prev = nodeToInsert;
+  
+        return removedTail;
+      }
+      return undefined;
     }
-
-    insertAfter(node: Node, nodeToInsert: Node) {
-        if (nodeToInsert === this.head && nodeToInsert === this.tail) return
-        this.remove(nodeToInsert)
-        nodeToInsert.prev = node;
-        nodeToInsert.next = node.next;
-        if (node.next === null) {
-            this.tail = nodeToInsert;
+  
+    print() {
+      let current = this.head;
+      while (current) {
+        console.log(
+          `${current.previous?.value} ${current.value} ${current.next?.value}`
+        );
+        current = current.next;
+      }
+    }
+  
+    // Insert node from the head instead of the tail
+  
+    insertHead(value: any) {
+      this.length++;
+      let newNode = new Node(value);
+  
+      if (this.head) {
+        this.head.previous = newNode;
+        newNode.next = this.head;
+        this.head = newNode;
+        return newNode;
+      }
+  
+      this.head = this.tail = newNode;
+      return newNode;
+    }
+  
+    // Remove node from the head instead of the tail
+    removeHead() {
+      if (this.head) {
+        this.length--;
+        const removedHead = this.head;
+        this.head = this.head.next;
+        if (this.head) {
+          this.head.previous = null;
         } else {
-            node.next.prev = nodeToInsert; 
+          this.tail = null;
         }
-        node.next = nodeToInsert
+        return removedHead;
+      }
+      return undefined;
     }
-
-    insertAtPosition(position: Number, nodeToInsert: Node) {
-        if (position === 1) {
-            this.setHead(nodeToInsert);
-            return;
-        }
-        let node = this.head;
-        let currentPosition = 1
-        while (node !== null && currentPosition++ !== position) node = node.next;
-        if (node !== null) {
-            this.insertBefore(node, nodeToInsert);
-        } else {
-            this.setTail(nodeToInsert);
-        }
+  
+    // insert at specific index
+  
+    insertIndex(value: any, index: number) {
+      if (index >= this.length) {
+        throw new Error("Insert index out of bounds");
+      }
+  
+      if (index === 0) {
+        return this.insertHead(value);
+      }
+  
+      this.length++;
+      let currentNode = this.head;
+      for (let i = 0; i < index; i++) {
+        currentNode = currentNode.next;
+      }
+      const previousNode = currentNode.previous;
+      const newNode = new Node(value);
+      newNode.next = currentNode;
+      newNode.previous = previousNode;
+      previousNode.next = newNode;
+      currentNode.previous = newNode;
+      return newNode;
     }
-
-    removeNodesWithValue(value: any) {
-        let node = this.head;
-        while (node !== null) {
-            const nodeToRemove = node;
-            node = node.next
-            if (nodeToRemove.value === value) this.remove(nodeToRemove);
-        }
+  
+    // remove at specific index
+  
+    removeIndex(index: number) {
+      if (index >= this.length) {
+        throw new Error("Remove index out of bounds");
+      }
+  
+      if (index === 0) {
+        return this.removeHead();
+      }
+  
+      this.length--;
+      let currentNode = this.head;
+      for (let i = 0; i < index; i++) {
+        currentNode = currentNode.next;
+      }
+      const previousNode = currentNode.previous;
+      const nextNode = currentNode.next;
+      previousNode.next = nextNode;
+      nextNode.previous = previousNode;
+      return currentNode;
     }
+  }
+  
 
-    remove(node: Node) {
-        if (node === this.head) this.head = this.head.next;
-        if (node === this.tail) this.tail
-        this.tail.prev
-        this.removeNodeBindings(node)
-    }
-
-    containsNodeWithValue(value: any) {
-        let node = this.head;
-        while (node !== null && node.value !== value) node = node.next
-        return node !== null;
-    }
-
-    removeNodeBindings(node: Node) {
-        if (node.prev !== null) node.prev.next = node.next;
-        if (node.next !== null) node.next.prev
-        node.prev = null
-        node.next = null
-    }
-
-
-}
 
 export {
-    Node, DoublyLinkedList
+     DoublyLinkedList
 }
