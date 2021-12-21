4c4bcae9f6754e45c814dfdf7d59e5d2a523c363
starfrogsplash <ali_khalil@hotmail.co.uk>
2021-12-21 02:25:11 +0000

    Changed: riversized variables

--- algorithms/graphs/riverSizes.ts
+++ algorithms/graphs/riverSizes.ts
@@ -1,28 +1,28 @@
 const riverSizes = (matrix: number[][]): number[] => {
     const sizes: number[] = []
-    const visited = matrix.map(row => row.map(value => false))
-    for (let i = 0; i < matrix.length; i++) {
-        for (let j = 0; j < matrix[i].length; j++) {
-            if (visited[i][j]) continue
-            traverseNode(i, j, matrix, visited, sizes)
+    const visited = matrix.map(currentRow => currentRow.map(value => false))
+    for (let currentRow = 0; currentRow < matrix.length; currentRow++) {
+        for (let currentCol = 0; currentCol < matrix[currentRow].length; currentCol++) {
+            if (visited[currentRow][currentCol]) continue
+            traverseNode(currentRow, currentCol, matrix, visited, sizes)
         }
     }
     return sizes
 }
 
-const traverseNode = (i: number, j: number, matrix: number[][], visited: boolean[][], sizes: number[]) => {
+const traverseNode = (currentRow: number, currentCol: number, matrix: number[][], visited: boolean[][], sizes: number[]) => {
     let currentRiverSize = 0;
-    const nodesToExplore = [[i, j]]
+    const nodesToExplore = [[currentRow, currentCol]]
 
     while (nodesToExplore.length) {
         const currentNode = nodesToExplore.pop()
-        i = currentNode ? currentNode[0] : -1
-        j = currentNode ? currentNode[1] : -1
-        if (visited[i][j]) continue
-        visited[i][j] = true;
-        if (matrix[i][j] === 0) continue;
+        currentRow = currentNode ? currentNode[0] : -1
+        currentCol = currentNode ? currentNode[1] : -1
+        if (visited[currentRow][currentCol]) continue
+        visited[currentRow][currentCol] = true;
+        if (matrix[currentRow][currentCol] === 0) continue;
         currentRiverSize++
-        const unvisitedNeighbors = getUnvistedNeighbours(i, j, matrix, visited)
+        const unvisitedNeighbors = getUnvistedNeighbours(currentRow, currentCol, matrix, visited)
         for (const neighbor of unvisitedNeighbors) {
             nodesToExplore.push(neighbor)
         }
@@ -31,12 +31,21 @@ const traverseNode = (i: number, j: number, matrix: number[][], visited: boolean
     if (currentRiverSize > 0) sizes.push(currentRiverSize);
 }
 
-const getUnvistedNeighbours = (i: number, j: number, matrix: number[][], visited: boolean[][]) => {
+const getUnvistedNeighbours = (currentRow: number, currentCol: number, matrix: number[][], visited: boolean[][]) => {
     const unvistedNeighbours: number[][] = []
-    if (i > 0 && !visited[i - 1][j]) unvistedNeighbours.push([i - 1, j])
-    if (i < matrix.length - 1 && !visited[i + 1][j]) unvistedNeighbours.push([i + 1, j])
-    if (j > 0 && !visited[i][j - 1]) unvistedNeighbours.push([i, j - 1])
-    if (j < matrix[0].length - 1 && !visited[i][j + 1]) unvistedNeighbours.push([i, j + 1])
+    const above = [currentRow - 1, currentCol]
+    const below = [currentRow + 1, currentCol]
+    const right = [currentRow, currentCol + 1]
+    const left = [currentRow, currentCol - 1]
+    const visitedAbove = visited[currentRow - 1] ? visited[currentRow - 1][currentCol] : undefined
+    const visitedBelow = visited[currentRow + 1] ? visited[currentRow + 1][currentCol] : undefined
+    const visitedLeft = visited[currentRow][currentCol - 1]
+    const visitedRight = visited[currentRow][currentCol + 1]
+
+    if (currentRow > 0 && !visitedAbove) unvistedNeighbours.push(above)
+    if (currentRow < matrix.length - 1 && !visitedBelow) unvistedNeighbours.push(below)
+    if (currentCol > 0 && !visitedLeft) unvistedNeighbours.push(left)
+    if (currentCol < matrix[0].length - 1 && !visitedRight) unvistedNeighbours.push(right)
     return unvistedNeighbours
 }
 
