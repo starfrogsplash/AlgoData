f329f92e900e3712f6f7d5e33c7ca6ee3a477f25
starfrogsplash <ali_khalil@hotmail.co.uk>
2021-12-26 21:34:21 +0000

    Added: Dynamic programming

--- algorithms/dynamicProgramming/minNumWaysToMakeChange/minNumWaysToMakeChange.spec.ts
+++ algorithms/dynamicProgramming/minNumWaysToMakeChange/minNumWaysToMakeChange.spec.ts
@@ -1,11 +1,11 @@
 import {minNumWaysToMakeChange} from './minNumWaysToMakeChange'
 
-fdescribe('minNumWaysToMakeChange', () => {
-    fit('returns 3', () => {
+describe('minNumWaysToMakeChange', () => {
+    it('returns 3', () => {
         expect(minNumWaysToMakeChange(7, [1,5,10])).toEqual(3);
     })
 
-    fit('returns 2', () => {
+    it('returns 2', () => {
         expect(minNumWaysToMakeChange(6, [1,2,4])).toEqual(2);
     })
 
